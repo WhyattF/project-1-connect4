76189fb4c8dff006bb15330967ffc9f6aa53e6de
Whyatt Foster <whyattfoster@gmail.com>
2019-06-17 14:56:15 -0400

    setting player turns

--- grid.js
+++ grid.js
@@ -1,10 +1,16 @@
 // Consolidate grid file into main file before launch
+
+
 class Gamegrid {
     constructor(selector) {
         this.rows = 6
         this.columns = 7
         this.selector = selector
         this.constructGrid()
+        this.isPlayer1Turn = true
+        this.isPlayer2Turn = false                        
+        this.executePlayerTurn()
+
     }
     
     constructGrid() {
@@ -15,19 +21,52 @@ class Gamegrid {
                 .addClass('row')
                 board.append(row)
             for (let columns = 0; columns < this.columns; columns++) {
-                const columns = $('<div>')
+                const column = $('<div></div>')
                     .addClass('empty column')
-                    .on('click', function() {
-                        let c = ('.empty')
-                        .addClass('red')
-                        .removeClass('empty')
+                    .on('click', function(){
+                        // console.log(this.isPlayer1Turn)
+                        this.executePlayerTurn()
 
                     })
-                row.append(columns)
+
+                  row.append(column)
             }
             board.append(row)
         }
+    } 
+
+
+    executePlayerTurn() {
+        if (this.isPlayer1Turn === true) {
+            $(this).addClass('red').removeClass('empty')
+            this.isPlayer1Turn = !this.isPlayer1Turn
+            this.isPlayer2Turn = !this.isPlayer2Turn
+
+        } else if (
+            this.isPlayer2Turn === true) {
+                $(this).addClass('black').removeClass('empty')
+                this.isPlayer2Turn = !this.isPlayer2Turn
+                this.isPlayer1Turn = !this.isPlayer1Turn
+                // .bind(this)
+            }  
+        }
+
+        
     }
+
+
+
+
+//   let colorChange = $('.empty column')
+
+
+//    console.log($('.empty column'))
+
+//    function clickEvent(){
+//     console.log(this)
+
+
+
     //  
     // eventListeners() {
     //     let board = $(this.selector)
@@ -35,5 +74,5 @@ class Gamegrid {
     //     board.on('mouseenter', '.empty.column', function(){
     //     console.log('here',this)
     //     })
-    } 
+
 
--- main.js
+++ main.js
@@ -2,3 +2,12 @@ $(document).ready(function() {
     // Create game board
     let gamegrid = new Gamegrid('.gamegrid')
 })
+
+// const changeColor = () => {
+//     console.log(this)
+//        (this).addClass('red').removeClass('empty')
+   
+//    }
+
+
+
